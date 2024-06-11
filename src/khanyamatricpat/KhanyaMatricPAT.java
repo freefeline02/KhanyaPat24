b8e26c1e8c956661ad2ac81d056fb65c2843653d
24khanyam <24khanyam@DESKTOP-1P0AG8J.Kingswood.local>
2024-06-11 17:08:14 +0845

    these is the most recent version of my UI's (10/06/24) edited individual UI's

---
+++ src/khanyamatricpat/KhanyaMatricPAT.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package khanyamatricpat;
+
+/**
+ *
+ * @author 24khanyam
+ */
+public class KhanyaMatricPAT {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       LandingPageUI lpd = new LandingPageUI();
+        lpd.setVisible(true);
+    }
+    
+}
